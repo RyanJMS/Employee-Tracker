39c7130d8e022e4cc90d101a18efdf2f9f24032e
RyanJMS <59546790+RyanJMS@users.noreply.github.com>
2020-06-01 16:30:21 -0400

    Finished other functions, working app

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ function main() {
           viewEmployees();
           break;
 
-        case "View Derpartments":
+        case "View Departments":
           viewDepartments();
           break;
 
@@ -81,29 +81,258 @@ function main() {
       }
     });
 
-  function viewEmployees() {
-    let query = `SELECT e.id, e.first_name, r.title, d.name AS department, r.salary
-    FROM employee e
-    LEFT JOIN role r
-    ON e.role_id = r.id
-    LEFT JOIN department d
-    ON d.id = r.department_id`;
+  function employeeList() {
+    let query = `SELECT * FROM employee`;
 
     connection.query(query, function (err, res) {
       if (err) throw err;
+
+      const employeeList = res.map(({ id, first_name, last_name }) => ({
+        value: id,
+        name: `${id} ${first_name} ${last_name}`,
+      }));
       console.table(res);
+    });
+  }
+
+  function departmentList() {
+    let query = `SELECT * FROM department`;
+
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+
+      const despartmentList = res.map(({ id, name }) => ({
+        value: id,
+        name: `${id} ${name}`,
+      }));
+      console.table(res);
+    });
+  }
+
+  function roleList() {
+    let query = `SELECT * FROM role`;
+
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+
+      const despartmentList = res.map(({ id, title, salary }) => ({
+        value: id,
+        name: `${id} ${title} ${salary}`,
+      }));
+      console.table(res);
+    });
+  }
+
+  function viewEmployees() {
+    connection.query("SELECT * FROM employee", function (err, data) {
+      console.table(data);
       main();
     });
   }
 
-  function viewDepartments() {}
+  function viewDepartments() {
+    connection.query("SELECT * FROM department", function (err, data) {
+      console.table(data);
+      main();
+    });
+  }
 
-  function viewRoles() {}
-  function addEmployee() {}
-  function addDepartment() {}
-  function addRole() {}
-  function updateRole() {}
-  function removeEmployee() {}
-  function removeDepartment() {}
-  function removeRole() {}
+  function viewRoles() {
+    connection.query("SELECT * FROM role", function (err, data) {
+      console.table(data);
+      main();
+    });
+  }
+
+  function addEmployee() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "firstName",
+          message: "What is the the employee's first name?",
+        },
+        {
+          type: "input",
+          name: "lastName",
+          message: "What is the employee's last name?",
+        },
+        {
+          type: "number",
+          name: "roleId",
+          message: "What is the employee's role id?",
+        },
+        {
+          type: "number",
+          name: "managerId",
+          message: "What is the Id of the employee's manager?",
+        },
+      ])
+      .then(function (res) {
+        connection.query(
+          "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)",
+          [res.firstName, res.lastName, res.roleId, res.managerId],
+          function (err, data) {
+            if (err) throw err;
+            console.table(
+              "Employee " + res.firstName + " " + res.lastName + " Added"
+            );
+            main();
+          }
+        );
+      });
+  }
+
+  function addDepartment() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is the name of the department you are adding?",
+        },
+      ])
+      .then(function (res) {
+        connection.query(
+          "INSERT INTO department (name) VALUES (?)",
+          [res.name],
+          function (err, data) {
+            if (err) throw err;
+            console.table(res.name + " successfully added!");
+            main();
+          }
+        );
+      });
+  }
+
+  function addRole() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "title",
+          message: "What is the name of the role?",
+        },
+        {
+          type: "number",
+          name: "salary",
+          message: "What is the salary of the role?",
+        },
+        {
+          type: "number",
+          name: "department_id",
+          message: "What is the department ID?",
+        },
+      ])
+      .then(function (res) {
+        connection.query(
+          "INSERT INTO role (title, salary, department_id) VALUES (?,?,?)",
+          [res.title, res.salary, res.department_id],
+          function (err, data) {
+            if (err) throw err;
+            console.table(res.title + " successfully added!");
+            main();
+          }
+        );
+      });
+  }
+
+  function updateRole() {
+    employeeList();
+
+    inquirer
+      .prompt([
+        {
+          message: "Which employee would you like to update? (First Name)",
+          type: "input",
+          name: "first_name",
+        },
+        {
+          message: "Please enter the new role ID",
+          type: "number",
+          name: "role_id",
+        },
+      ])
+      .then(function (response) {
+        connection.query(
+          "UPDATE employee SET role_id = ? WHERE first_name = ?",
+          [response.role_id, response.first_name],
+          function (err, data) {
+            if (err) throw err;
+            console.table(data);
+            main();
+          }
+        );
+      });
+  }
+
+  function removeEmployee() {
+    employeeList();
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "firstName",
+          message: "Which employee would you like to remove? (by first name)",
+        },
+      ])
+      .then(function (res) {
+        let query = `DELETE FROM employee WHERE ?`;
+
+        connection.query(query, { first_name: res.firstName }, function (
+          err,
+          res
+        ) {
+          if (err) throw err;
+          console.table(res);
+          console.log(res.affectedRows + " Deleted");
+          main();
+        });
+      });
+  }
+  function removeDepartment() {
+    departmentList();
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "departmentName",
+          message: "Which department would you like to remove? (by name)",
+        },
+      ])
+      .then(function (res) {
+        let query = `DELETE FROM department WHERE ?`;
+
+        connection.query(query, { name: res.departmentName }, function (
+          err,
+          res
+        ) {
+          if (err) throw err;
+          console.table(res);
+          console.log(res.affectedRows + " Deleted");
+          main();
+        });
+      });
+  }
+  function removeRole() {
+    roleList();
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "roleName",
+          message: "Which role would you like to remove? (by title)",
+        },
+      ])
+      .then(function (res) {
+        let query = `DELETE FROM role WHERE ?`;
+
+        connection.query(query, { title: res.roleName }, function (err, res) {
+          if (err) throw err;
+          console.table(res);
+          console.log(res.affectedRows + " Deleted");
+          main();
+        });
+      });
+  }
 }
--- db/employee_db.sql
+++ db/employee_db.sql
@@ -6,7 +6,7 @@ USE employee_db;
 
 CREATE TABLE employee
 (
-        id INTEGER(100) NOT NULL,
+        id INTEGER(100) NOT NULL AUTO_INCREMENT,
         first_name VARCHAR
         (30),
         last_name VARCHAR
@@ -21,7 +21,7 @@ CREATE TABLE employee
 
 CREATE TABLE department
 (
-        id INTEGER(11) NOT NULL,
+        id INTEGER(11) NOT NULL AUTO_INCREMENT,
         name VARCHAR
                 (30),
         PRIMARY KEY
@@ -31,7 +31,7 @@ CREATE TABLE department
 CREATE TABLE role
 (
         id INTEGER(100)
-                NOT NULL,
+                NOT NULL AUTO_INCREMENT,
         title VARCHAR
                         (30) ,
         salary DECIMAL
@@ -41,3 +41,28 @@ CREATE TABLE role
         PRIMARY KEY
                         (id)
 );
+INSERT INTO employee_db.employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ("Mark", "Coleman", 2, null),
+    ("Brock", "Lesnar", 2, null),
+    ("Dan", "Gable", 1, null),
+    ("Sean", "O'Malley", 1, null),
+    ("Valentina", "Schevchenko", 1, null),
+    ("Holly", "Holm", 3, null),
+    ("Nieky", "Holzken", 1, null),
+    ("Michelle", "Waterson", 3, null);
+
+INSERT INTO employee_db.department
+    (name)
+VALUES
+    ("Sales"),
+    ("Customer Service"),
+    ("Human Resources");
+
+INSERT INTO employee_db.role
+    (title, salary, department_id)
+VALUES
+    ("Sales Associate", 50000, 1),
+    ("Customer Service Associate", 50000, 2),
+    ("Human Resources", 60000, 3);  
